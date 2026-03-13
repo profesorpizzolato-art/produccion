f201e1503dbc04a2d9cfe02c4559fd8e8086dedb
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-13 17:46:47 -0300

    Create banco_preguntas.py

---
+++ banco_preguntas.py
@@ -0,0 +1,13 @@
+preguntas = [
+
+{
+"pregunta": "Función del separador trifásico",
+"opciones": [
+"Separar petróleo gas agua",
+"Aumentar presión",
+"Reducir temperatura"
+],
+"respuesta":0
+},
+
+]
