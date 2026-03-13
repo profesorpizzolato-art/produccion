d4469040ae86ba25509f47e6f9c1fdc4c3c205fa
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-13 17:45:57 -0300

    Update simulador_fallas.py

--- simulador_fallas.py
+++ simulador_fallas.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import random
 
+def fallas():
+
+    eventos = [
+
+    "Slugging en línea",
+    "Gas lock",
+    "Cavitación bomba",
+    "Alta presión separador",
+    "Bloqueo línea"
+
+    ]
+
+    if st.button("Simular incidente"):
+
+        evento = random.choice(eventos)
+
+        st.error(evento)
