9a60c937804d5153619825b39be16b5f84862c8a
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-15 18:05:15 -0300

    Create dashboard_campo.py

---
+++ dashboard_campo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dashboard_campo():
+
+    st.title("Centro de Control de Producción")
+    st.subheader("Simulador MENFA - Campo Petrolero")
+
+    st.markdown("---")
+
+    st.sidebar.header("Configuración del Campo")
