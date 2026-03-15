66dc5dfbb1fde6b8adf5221c8a4e700db20f4728
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-15 17:17:07 -0300

    Update bomba_mecanica.py

--- modulos/bomba_mecanica.py
+++ modulos/bomba_mecanica.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+def bomba_mecanica():
+
+    st.title("Simulador de Bombeo Mecánico")
+
+    carrera = st.slider("Carrera bomba (in)",10,200,80)
+    spm = st.slider("Golpes por minuto",1,20,8)
+
+    eficiencia = st.slider("Eficiencia (%)",40,100,75)
+
+    produccion = carrera * spm * eficiencia * 0.1
+
+    st.metric("Producción estimada BPD",round(produccion))
