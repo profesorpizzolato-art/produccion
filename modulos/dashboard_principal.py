6ac77c764f345592df0fd5e03f1036f0757574a8
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-15 18:17:38 -0300

    Create dashboard_principal.py

---
+++ modulos/dashboard_principal.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+def dashboard_principal():
+
+    st.title("SIMULADOR OPERATIVO MENFA")
+    st.subheader("Centro de Entrenamiento en Producción Petrolera")
+
+    st.markdown("---")
+
+    st.write("Seleccione un módulo:")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        if st.button("Pozo Productor"):
+            st.session_state.modulo = "pozo"
+
+        if st.button("SCADA Planta"):
+            st.session_state.modulo = "planta"
+
+    with col2:
+        if st.button("Diagrama Planta"):
+            st.session_state.modulo = "diagrama"
+
+        if st.button("Fórmulas Producción"):
+            st.session_state.modulo = "formulas"
+
+    with col3:
+        if st.button("IPR - VLP"):
+            st.session_state.modulo = "ipr"
+
+        if st.button("Evaluación"):
+            st.session_state.modulo = "evaluacion"
