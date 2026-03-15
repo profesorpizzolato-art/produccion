2c326f24861132bd3550ec0d773637dd312d0656
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-15 16:41:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,88 @@
 import streamlit as st
 
+# IMPORTAR MODULOS
+
+from modulos.dashboard import dashboard
 from modulos.panel_operador import panel_operador
-from modulos.scada import scada
-from modulos.campo_pozos import campo
-from modulos.manifold import manifold
-from modulos.separador import separador
-from modulos.bomba_superficie import bomba
-from modulos.linea_flujo import linea
-from modulos.tanques import tanques
-from modulos.compresor_gas import compresor
+from modulos.campo_petrolero import campo
+from modulos.scada_planta import scada_planta
+from modulos.scada_avanzado import scada
+from modulos.pozo_productor import pozo_productor
+from modulos.modelo_reservorio import reservorio
+from modulos.flujo_multifasico import flujo
+from modulos.manifold_avanzado import manifold
+from modulos.eficiencia_separador import separador
 from modulos.tendencias import tendencias
-from modulos.balance_produccion import balance
-from modulos.alarmas import alarmas
-from modulos.simulador_fallas import fallas
+from modulos.corte_agua import corte_agua
+from modulos.choke_control import choke
+from modulos.presiones_pozo import presiones
+from modulos.simulacion_produccion import simulacion
+from modulos.alarmas_scada import alarmas
+from modulos.eventos_operativos import eventos
 
-st.set_page_config(layout="wide")
 
-st.title("SIMULADOR DE PRODUCCIÓN PETROLERA")
-st.subheader("IPCL MENFA – Centro de Entrenamiento")
+# CONFIGURACION
 
-menu = st.sidebar.selectbox(
-"Menú",
-[
-"Panel Operador",
-"SCADA",
-"Campo Pozos",
-"Manifold",
-"Separador",
-"Bomba",
-"Línea Flujo",
-"Tanques",
-"Compresor",
-"Tendencias",
-"Balance Producción",
-"Alarmas",
-"Fallas"
-]
+st.set_page_config(
+    page_title="Simulador Producción MENFA",
+    layout="wide"
 )
 
-if menu == "Panel Operador":
+# TITULO
+
+st.title("SIMULADOR DE PRODUCCIÓN PETROLERA")
+st.subheader("IPCL MENFA - Centro de Entrenamiento Operativo")
+
+# MENU
+
+menu = st.sidebar.selectbox(
+    "Menú del simulador",
+    [
+        "Dashboard",
+        "Panel Operador",
+        "Campo Petrolero",
+        "SCADA Planta",
+        "SCADA Avanzado",
+        "Pozo Productor",
+        "Modelo Reservorio",
+        "Flujo Multifásico",
+        "Manifold",
+        "Separador",
+        "Tendencias",
+        "Corte Agua",
+        "Control Choke",
+        "Presiones Pozo",
+        "Simulación Producción",
+        "Alarmas",
+        "Eventos Operativos"
+    ]
+)
+
+# EJECUCION MODULOS
+
+if menu == "Dashboard":
+    dashboard()
+
+elif menu == "Panel Operador":
     panel_operador()
 
-elif menu == "SCADA":
+elif menu == "Campo Petrolero":
+    campo()
+
+elif menu == "SCADA Planta":
+    scada_planta()
+
+elif menu == "SCADA Avanzado":
     scada()
 
-elif menu == "Campo Pozos":
-    campo()
+elif menu == "Pozo Productor":
+    pozo_productor()
+
+elif menu == "Modelo Reservorio":
+    reservorio()
+
+elif menu == "Flujo Multifásico":
+    flujo()
 
 elif menu == "Manifold":
     manifold()
@@ -53,39 +90,23 @@ elif menu == "Manifold":
 elif menu == "Separador":
     separador()
 
-elif menu == "Bomba":
-    bomba()
-
-elif menu == "Línea Flujo":
-    linea()
-
-elif menu == "Tanques":
-    tanques()
-
-elif menu == "Compresor":
-    compresor()
-
 elif menu == "Tendencias":
     tendencias()
 
-elif menu == "Balance Producción":
-    balance()
+elif menu == "Corte Agua":
+    corte_agua()
+
+elif menu == "Control Choke":
+    choke()
+
+elif menu == "Presiones Pozo":
+    presiones()
+
+elif menu == "Simulación Producción":
+    simulacion()
 
 elif menu == "Alarmas":
     alarmas()
 
-elif menu == "Fallas":
-    fallas()
-from modulos.tendencias import tendencias
-from modulos.corte_agua import corte_agua
-from modulos.choke_control import choke
-from modulos.presiones_pozo import presiones
-from modulos.alarmas_scada import alarmas
-from modulos.simulacion_produccion import simulacion
-from modulos.dashboard import dashboard
-from modulos.modelo_reservorio import reservorio
-from modulos.flujo_multifasico import flujo
-from modulos.manifold_avanzado import manifold
-from modulos.eficiencia_separador import separador
-from modulos.eventos_operativos import eventos
-from modulos.scada_avanzado import scada
+elif menu == "Eventos Operativos":
+    eventos()
