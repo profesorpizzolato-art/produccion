fb2fb2e868ddc78f46ac98634c8414655d969026
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-15 18:31:23 -0300

    Update dashboard_principal.py

--- modulos/dashboard_principal.py
+++ modulos/dashboard_principal.py
@@ -7,27 +7,30 @@ def dashboard_principal():
 
     st.markdown("---")
 
-    st.write("Seleccione un módulo:")
+    st.write("Seleccione un módulo")
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        if st.button("Pozo Productor"):
+
+        if st.button("🛢 Pozo Productor"):
             st.session_state.modulo = "pozo"
 
-        if st.button("SCADA Planta"):
+        if st.button("🏭 SCADA Planta"):
             st.session_state.modulo = "planta"
 
     with col2:
-        if st.button("Diagrama Planta"):
+
+        if st.button("📊 Diagrama Planta"):
             st.session_state.modulo = "diagrama"
 
-        if st.button("Fórmulas Producción"):
+        if st.button("📈 Fórmulas Producción"):
             st.session_state.modulo = "formulas"
 
     with col3:
-        if st.button("IPR - VLP"):
+
+        if st.button("📉 IPR - VLP"):
             st.session_state.modulo = "ipr"
 
-        if st.button("Evaluación"):
+        if st.button("🎓 Evaluación"):
             st.session_state.modulo = "evaluacion"
