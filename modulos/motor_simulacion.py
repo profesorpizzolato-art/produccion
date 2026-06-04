3bb8c72283285009e21ff228ad3f89775f26eb51
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-06-03 22:23:21 -0300

    Refactor MotorSimulacion methods for clarity

--- modulos/motor_simulacion.py
+++ modulos/motor_simulacion.py
@@ -2,39 +2,38 @@ import random
 import numpy as np
 
 class MotorSimulacion:
-# Dentro de la clase MotorSimulacion
-def __init__(self):
-    self.presion = 115.0
-    self.nivel = 14.2
-    self.caudal_base = 450.0
-    self.historial = []
-    self.esd_activo = False  # Estado de la planta
+    def __init__(self):
+        self.presion = 115.0
+        self.nivel = 14.2
+        self.caudal_base = 450.0
+        self.historial = []
+        self.esd_activo = False  # Estado de la planta
 
-def obtener_datos(self):
-    if self.esd_activo:
-        # En ESD, la presión sube por el cierre y el caudal cae a cero
-        self.presion = min(self.presion + 0.1, 160.0) 
+    def obtener_datos(self):
+        if self.esd_activo:
+            # En ESD, la presión sube por el cierre y el caudal cae a cero
+            self.presion = min(self.presion + 0.1, 160.0) 
+            self.caudal_base = 0.0
+        else:
+            self.presion += random.uniform(-0.5, 0.5)
+            self.caudal_base += random.uniform(-1.0, 1.0)
+        
+        return {
+            "presion": round(self.presion, 2),
+            "nivel": round(self.nivel, 2),
+            "caudal": round(self.caudal_base, 2)
+        }
+
+    def activar_esd(self):
+        """Cierra las SDV (Válvulas de Seguridad)"""
+        self.esd_activo = True
         self.caudal_base = 0.0
-    else:
-        self.presion += random.uniform(-0.5, 0.5)
-        self.caudal_base += random.uniform(-1.0, 1.0)
-    
-    return {
-        "presion": round(self.presion, 2),
-        "nivel": round(self.nivel, 2),
-        "caudal": round(self.caudal_base, 2)
-    }
 
-def activar_esd(self):
-    """Cierra las SDV (Válvulas de Seguridad)"""
-    self.esd_activo = True
-    self.caudal_base = 0.0
-
-def reset_planta(self):
-    """Normaliza la planta después de un ESD"""
-    self.esd_activo = False
-    self.presion = 115.0
-    self.caudal_base = 450.0
+    def reset_planta(self):
+        """Normaliza la planta después de un ESD"""
+        self.esd_activo = False
+        self.presion = 115.0
+        self.caudal_base = 450.0
 
     def evolucion_produccion(self):
         """Genera el array de datos que requiere el gráfico de scada.py"""
